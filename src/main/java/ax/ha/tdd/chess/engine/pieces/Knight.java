64e1b3064527deba1b5353d327bd162dd93b3a2b
Mathias Sjöblom <mathias.sjoblom@ha.ax>
2022-04-02 00:34:01 +0300

    Knight test

--- src/main/java/ax/ha/tdd/chess/engine/pieces/Knight.java
+++ src/main/java/ax/ha/tdd/chess/engine/pieces/Knight.java
@@ -1,4 +1,25 @@
 package ax.ha.tdd.chess.engine.pieces;
 
-public class Knight {
+import ax.ha.tdd.chess.engine.Chessboard;
+import ax.ha.tdd.chess.engine.Coordinates;
+import ax.ha.tdd.chess.engine.Player;
+
+public class Knight extends ChessPiece{
+
+    public Knight(PieceType pieceType, Player player, Coordinates location) {
+        super(pieceType, player, location);
+    }
+
+    @Override
+    public String getSymbol() {
+        return String.valueOf(PieceType.KNIGHT);
+    }
+
+    @Override
+    public boolean canMove(Chessboard chessboard, Coordinates destination) {
+
+
+
+        return false;
+    }
 }
---
+++ src/test/java/ax/ha/tdd/chess/engine/KnightTest.java
@@ -0,0 +1,19 @@
+package ax.ha.tdd.chess.engine;
+
+import ax.ha.tdd.chess.engine.pieces.King;
+import ax.ha.tdd.chess.engine.pieces.Knight;
+import ax.ha.tdd.chess.engine.pieces.PieceType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KnightTest {
+
+    @Test
+    public void testKnight() {
+        String moveFrom = "d5";
+        Coordinates testCoords = new Coordinates(moveFrom);
+        assertEquals("WHITE Knight", new Knight(PieceType.KNIGHT, Player.WHITE, testCoords));
+    }
+
+}
