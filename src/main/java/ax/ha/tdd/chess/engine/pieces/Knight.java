ec617de6167eafb62544601f743df3b1d3f2c5a4
Mathias Sjöblom <mathias.sjoblom@ha.ax>
2022-04-07 02:25:10 +0300

    knight move & test

--- src/main/java/ax/ha/tdd/chess/engine/pieces/Knight.java
+++ src/main/java/ax/ha/tdd/chess/engine/pieces/Knight.java
@@ -18,7 +18,48 @@ public class Knight extends ChessPiece{
     @Override
     public boolean canMove(Chessboard chessboard, Coordinates destination) {
 
+        Boolean moveable = true;
+        Boolean notMoveable = false;
 
+        if(destination == location) {
+            return notMoveable;
+        }
+
+        ChessPiece pieceCheck = chessboard.getPiece(destination);
+        if(pieceCheck != null && pieceCheck.getPlayer().getSymbol() == this.player.getSymbol()) {
+            return notMoveable;
+        }
+
+        int x = location.getX();
+        int y = location.getY();
+        int destX = destination.getX();
+        int destY = destination.getY();
+
+        if(destX == (x+2) && destY == (y+1)) {
+            return moveable;
+        }
+        if(destX == (x+1) && destY == (y+2)) {
+            return moveable;
+        }
+        if(destX == (x+2) && destY == (y-1)) {
+            return moveable;
+        }
+        if(destX == (x+1) && destY == (y-2)) {
+            return moveable;
+        }
+
+        if(destX == (x-2) && destY == (y+1)) {
+            return moveable;
+        }
+        if(destX == (x-1) && destY == (y+2)) {
+            return moveable;
+        }
+        if(destX == (x-2) && destY == (y-1)) {
+            return moveable;
+        }
+        if(destX == (x-1) && destY == (y-2)) {
+            return moveable;
+        }
 
         return false;
     }
--- src/test/java/ax/ha/tdd/chess/engine/KnightTest.java
+++ src/test/java/ax/ha/tdd/chess/engine/KnightTest.java
@@ -1,8 +1,6 @@
 package ax.ha.tdd.chess.engine;
 
-import ax.ha.tdd.chess.engine.pieces.King;
-import ax.ha.tdd.chess.engine.pieces.Knight;
-import ax.ha.tdd.chess.engine.pieces.PieceType;
+import ax.ha.tdd.chess.engine.pieces.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -21,6 +19,28 @@ public class KnightTest {
         String moveFrom = "d5";
         Coordinates testCoords = new Coordinates(moveFrom);
         assertEquals("KNIGHT", new Knight(PieceType.KNIGHT, Player.WHITE, testCoords).getSymbol());
+    }
+
+    @Test
+    public void testRookMove() {
+        Chessboard board = new Chessboard();
+        String moveFrom = "a3";
+        String moveTo= "b5";
+        Coordinates testCoords = new Coordinates(moveFrom);
+        Coordinates testCoords2 = new Coordinates(moveTo);
+        assertEquals(true, new Knight(PieceType.KNIGHT, Player.WHITE, testCoords).canMove(board, testCoords2));
+
+        String moveTo2= "a5";
+        Coordinates testCoords22 = new Coordinates(moveTo2);
+        assertEquals(false, new Knight(PieceType.KNIGHT, Player.WHITE, testCoords).canMove(board, testCoords22));
+
+        //Try stepping on a tile with a friendly piece on
+        String pieceBlock= "b5";
+        Coordinates blockCoords = new Coordinates(pieceBlock);
+        ChessPiece testPiece = new Knight(PieceType.KNIGHT, Player.BLACK, blockCoords);
+        board.addPiece(testPiece);
+        assertEquals(false, new Knight(PieceType.KNIGHT, Player.BLACK, testCoords).canMove(board, blockCoords));
+
     }
 
 }
